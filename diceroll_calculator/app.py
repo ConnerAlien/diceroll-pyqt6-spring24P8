ffbf61f56be08cf173efd79aa10b9d87411dacf1
ConnerAlien <powec169@hsd.k12.or.us>
2024-06-12 14:25:45 -0700

    bug fixes

--- diceroll_calculator/app.py
+++ diceroll_calculator/app.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 from PyQt6.QtGui import QFont, QFontDatabase
 from PyQt6.QtWidgets import (
@@ -31,6 +32,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Rando")
         self.setContentsMargins(12, 40, 12, 12)
         self.resize(320, 240)
+        #self.setSizePolicy()
 
         #install fonts
         self.set_font("Comfortaa-Light.ttf")
@@ -43,16 +45,29 @@ class MainWindow(QMainWindow):
         Label_1 = QLabel("How many dice do you want to roll?")
         Label_1.setFont(QFont("Comfortaa-Light.ttf", 12))
 
-        Input_1 = QSpinBox()
-        Input_1.setFont(QFont("Comfortaa-Light.ttf", 12))
+        self.Input_1 = QSpinBox()
+        self.Input_1.setFont(QFont("Comfortaa-Light.ttf", 12))
 
         roll_button = QPushButton("Roll")
         roll_button.setFont(QFont("Comfortaa-Light.ttf", 12, 4))
+        roll_button.clicked.connect(self.roll_dice)
+
+        self.roll_result = QLabel("")
+        self.roll_result.setFont(QFont("Comfortaa-Light.ttf", 12, 4))
+        
+        self.roll_sum = QLabel("")
+        self.roll_sum.setFont(QFont("Comfortaa-Light.ttf", 12, 4))
+
+        self.roll_product = QLabel("")
+        self.roll_product.setFont(QFont("Comfortaa-Light.ttf", 12, 4))
 
         layout.addWidget(title_label)
         layout.addWidget(Label_1)
-        layout.addWidget(Input_1)
+        layout.addWidget(self.Input_1)
         layout.addWidget(roll_button)
+        layout.addWidget(self.roll_result)
+        layout.addWidget(self.roll_sum)
+        layout.addWidget(self.roll_product)
         layout.addStretch()
 
         widget = QWidget()
@@ -69,6 +84,27 @@ class MainWindow(QMainWindow):
         #If it failed to add the font
         if success == -1:
             print(f"{font_name} not loaded. \nTried path '{font_path}'")
+
+    def roll_dice(self):
+        sum = 0
+        product = 1
+        roll = 1
+
+        title = ""
+
+        for i in range(self.Input_1.value()):
+            roll = random.randrange(1,7)
+            sum += roll
+            product = product * roll
+            title += "roll " + str(i + 1) + ": " + str(roll) + " | "
+            print(title)
+
+        self.roll_result.setText(title)
+        self.roll_sum.setText(f"Sum of rolls: {sum}")
+        self.roll_product.setText(f"Product of rolls: {product}")
+
+
+
 
 app = QApplication(sys.argv)
 
